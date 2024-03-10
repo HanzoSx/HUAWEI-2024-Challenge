08e5a21b926551ffe3c73f2aa264c3ab5ac9856b
nn <2949691340@qq.com>
2024-03-10 15:45:13 +0800

    create

--- classMap.hpp
+++ classMap.hpp
@@ -1,12 +1,12 @@
 #ifndef CLASS_MAP_H
 #define CLASS_MAP_H
 
+#include<queue>
 #include "constList.hpp"
-
 char ch[N][N];
 bool walkable(int x, int y)
 {
-    return ch[x][y] != '*' and ch[x][y] != '#';
+    return x >= 0 and x < c_size and y>0 and y<c_size and ch[x][y] != '*' and ch[x][y] != '#';
 }
 
 class DisMap
@@ -38,7 +38,26 @@ void DisMap::setZeroRect(int x, int y, int width, int height)
 
 void DisMap::creat()
 {
-    
+    pair<int, int> t;
+    int x, y;
+    queue < pair<int, int> > Q;
+    for (int x = 0; x < c_size; ++ x)
+    for (int y = 0; y < c_size; ++ y)
+        if (dis[x][y] == 0)
+            Q.push(make_pair(x, y));
+    while (!Q.empty())
+    {
+        t=Q.front();
+        Q.pop();
+        x=t.first;
+        y=t.second;
+        for (int i = 0; i < 4; ++ i)
+            if (walkable(x + c_dir[i][0], y + c_dir[i][1]) and dis[x][y] + 1 < dis[x + c_dir[i][0]][y + c_dir[i][1]])
+            {
+                Q.push(make_pair(x + c_dir[i][0], y + c_dir[i][1]));
+                dis[x + c_dir[i][0]][y + c_dir[i][1]] = dis[x][y] + 1;
+            }
+    }
 }
 
 
